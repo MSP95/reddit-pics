85b7069151333171d10f0bfdc4ac89da4441427b
MSP95 <patilmanish1910@gmail.com>
2019-03-06 23:57:49 -0500

    Updates

---
+++ precache-manifest.3f7e5936d848f9923ec07b9000e0b98a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea7441ae58939a9e078",
+    "url": "/reddit-pics/static/js/runtime~main.6ea7441a.js"
+  },
+  {
+    "revision": "75b22591e4fdc2c72bff",
+    "url": "/reddit-pics/static/js/main.75b22591.chunk.js"
+  },
+  {
+    "revision": "31a5bbcdaa2bf99ee666",
+    "url": "/reddit-pics/static/js/2.31a5bbcd.chunk.js"
+  },
+  {
+    "revision": "75b22591e4fdc2c72bff",
+    "url": "/reddit-pics/static/css/main.1ac81b4e.chunk.css"
+  },
+  {
+    "revision": "28485842a0d0f5d473107fab37b70972",
+    "url": "/reddit-pics/index.html"
+  }
+];
